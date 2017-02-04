8d6702ac798ffe008171e8a9f3a8eb8430f8eae2
demon song <787423495@qq.com>
2017-02-04 20:52:01 +0800

    添加博客界面

--- server.js
+++ server.js
@@ -18,6 +18,19 @@ app.get('/',function (req,res){
     });
 });
 
+app.get('/blog',function (req,res){
+    fs.readFile('blog.html',function(err,data){
+        if(err){
+            console.log(err);
+            res.writeHead(404,{'Content-Type': 'text/html'});
+        }else{
+            res.writeHead(200, {'Content-Type': 'text/html'});  
+            res.write(data.toString());
+        }
+        res.end();
+    });
+});
+
 //express route
 app.get('/test',function (req,res){
     fs.readFile('guide.html',function(err,data){
