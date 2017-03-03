6f1467247c52fcc728848f3ceda836c645ae86da
demon song <787423495@qq.com>
2017-03-03 19:03:42 +0800

    完成文章样式排版

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,5 @@
-var index = -1,
-	timer = null;
+(function($) {
 
+	console.log("hello world");
 
-function imagePlayer(){
-	var image = document.getElementsByTagName('img');
-	index ++;
-	if(index == image.length){
-		index = 0;
-	}
-	clearImage(image);
-	image[index].style.display = "block";
-	
-}
-
-function clearImage(image){
-	for(var i = 0; i < image.length; i++){
-		if(i == index){
-			continue;
-		}
-		image[i].style.display = "none";
-	}
-}
-
-function startImagePlayer(){
-	//实现照片轮播
-	clearInterval(timer);
-	setInterval(imagePlayer,150);
-}
-
+})(jQuery);
